eac0a6b008c2f20eb25f4a8748f38fbc4f4f88d7
bulelani <mbali.bulelani@gmail.com>
2021-11-13 10:07:55 +0200

    Revert "react basics"
    This reverts commit 5331021818de110cde7fe6f4ce8a8cbc7dfc9028.

--- src/components/Backdrop.js
+++
@@ -1,8 +0,0 @@
-function Backdrop(prop) {
-
-  return (
-    <div className="backdrop" onClick={prop.cancel} />
-
-  );
-}
-export default Backdrop;
--- src/index.js
+++ src/index.js
@@ -2,18 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Todo from './components/Todo';
-import Modal from './components/Modal';
-import Backdrop from './components/Backdrop';
-
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <di>
+  <React.StrictMode>
     <App />
-    <Todo title='I am the king title' />
-    <Todo title="this is a new title" />
-  </di>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
