5331021818de110cde7fe6f4ce8a8cbc7dfc9028
bulelani <mbali.bulelani@gmail.com>
2021-11-13 09:41:24 +0200

    react basics

---
+++ src/components/Backdrop.js
@@ -0,0 +1,8 @@
+function Backdrop(prop) {
+
+  return (
+    <div className="backdrop" onClick={prop.cancel} />
+
+  );
+}
+export default Backdrop;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Todo from './components/Todo';
+import Modal from './components/Modal';
+import Backdrop from './components/Backdrop';
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <di>
     <App />
-  </React.StrictMode>,
+    <Todo title='I am the king title' />
+    <Todo title="this is a new title" />
+  </di>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
